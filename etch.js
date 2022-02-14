9291b8c56fba7e9b29795a757d4cc096b10d62ce
Steven McKeown <stevenmckeown19@gmail.com>
2022-02-14 15:38:32 +0000

    added border to container, removed margin and padding from col and row, add black hover effect to cols and added col height and width

--- etch.js
+++ etch.js
@@ -4,15 +4,15 @@ const container = document.querySelector('.container');
 function createGrid() {
     for(let rows = 0; rows < 16; rows++) {
         const rows = document.createElement('div');
-            rows.textContent = 'rows';
+            
             rows.classList.add('row');
             container.appendChild(rows);
 
-        for(let cols = 0; cols < 16; cols++) {
-            const cols = document.createElement('div');
-            cols.classList.add('col')
-            cols.textContent = 'cols';
-            rows.appendChild(cols);
+            for(let cols = 0; cols < 16; cols++) {
+                const cols = document.createElement('div');
+                cols.classList.add('col')
+                // cols.textContent = 'cols';
+                rows.appendChild(cols);
         }
     }
 }
