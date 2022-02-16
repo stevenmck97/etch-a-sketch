d28f8e21569904c6d1da40d607e53b8fe8a81641
Steven McKeown <stevenmckeown19@gmail.com>
2022-02-16 19:08:44 +0000

    Added rainbowDraw function to return 3 random rgb values. Removed draw function and set background colour in createGrid function instead. Created handleColor function to change draw style.

--- etch.js
+++ etch.js
@@ -1,19 +1,20 @@
 const container = document.querySelector('.container');
+rainbowCount = 0;
+colCount = 0;
+
+(function defaultGrid() {
+    createGrid(64); 
+})();
+
+
 
 function size() {
    sizePrompt = prompt('What size would you like?');
    deleteGrid()
    createGrid(sizePrompt)
-   draw();
+   
     
 }
-
-(function defaultGrid() {
-    createGrid(64);
-    draw();
-})();
-
-
 
 function createGrid(gridSize) {
     if (gridSize <= 100 && gridSize >= 1) {
@@ -24,16 +25,26 @@ function createGrid(gridSize) {
     for(let rows = 0; rows < gridSize; rows++) {
         const rows = document.createElement('div');
         rows.classList.add('row');
+        // rows.addEventListener('mouseover', rainbowDraw)
         container.appendChild(rows);
-
+        
         for(let cols = 0; cols < gridSize; cols++) {
             const cols = document.createElement('div');
             cols.classList.add('col')
+
+            if(rainbowCount == 0) {
+                cols.addEventListener('mouseover', () => {
+                    cols.style.backgroundColor = "black"
+                })
+            } else {
+                cols.addEventListener('mouseover', () => {
+                    cols.style.backgroundColor = rainbowDraw()
+                })
+            }
             rows.appendChild(cols);
+            
     }
 }};
-
-
 
 function deleteGrid() {
     const rows = document.querySelectorAll('.row')
@@ -47,13 +58,25 @@ function deleteGrid() {
     })
     }
 
-function draw() {
-    const cols = document.querySelectorAll('.col');
-    cols.forEach((col) => {
-        col.addEventListener('mouseover', () => {
-            col.style.backgroundColor = "black"
-        })
-    })
+function rainbowDraw() {
+    x  = Math.floor(Math.random() * 256);
+    y  = Math.floor(Math.random() * 256);
+    z  = Math.floor(Math.random() * 256);
+    
+    return `rgb(${x}, ${y}, ${z})`  
+}
+
+function handleColor() {
+    const rainbowBtn = document.querySelector('#rainbowBtn')
+    if (rainbowCount === 0){
+        rainbowCount+=1
+        rainbowBtn.style.backgroundColor= 'blue'
+    } else if(rainbowCount===1){
+        rainbowCount-=1
+        rainbowBtn.style.backgroundColor= 'transparent'
+    }
+    deleteGrid()
+    createGrid()
 }
 
 function clearDrawing() {
@@ -63,7 +86,7 @@ function clearDrawing() {
     }) 
 }
 
-
+//window.onload
 
 // function draw(){
 //     const cols = document.querySelectorAll('.col');
@@ -73,6 +96,27 @@ function clearDrawing() {
 //             cols[i].style.backgroundColor = "black"
 //         })
 //     }
+// }
+
+// function draw() {
+//     const cols = document.querySelectorAll('.col');
+//     cols.forEach((col) => {
+//         col.addEventListener('mouseover', () => {
+//             col.style.backgroundColor = "black"
+//         })
+//     })
+// }
+
+// function rainbowDraw() {
+//     x  = Math.floor(Math.random() * 256);
+//     y  = Math.floor(Math.random() * 256);
+//     z  = Math.floor(Math.random() * 256);
+//     const cols = document.querySelectorAll('.col');
+//     cols.forEach((col) => {
+//         col.addEventListener('mouseover', () => {
+//             col.style.backgroundColor = `rgb(${x}, ${y}, ${z})`
+//         })
+//     })
 // }
 
 
